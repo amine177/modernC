7a19f96a6ee407c8c5d3c8276bb70ab2e211b4fd
aminos <amri.amine@protonmail.com>
2019-09-03 19:34:55 +0100

    using a prefix block notation every { goes in the same line as the block's name

---
+++ ch1/prefix-block-notation.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+
+
+int main(int argc, char* argv[argc+1]) {
+  puts("Hello world!");
+  if (argc > 1) {
+    while (true) {
+      puts("some programs never stop");
+    }
+  } else {
+    do {
+      puts("but this one does");
+    } while (false);
+  }
+  return EXIT_SUCCESS;
+}
